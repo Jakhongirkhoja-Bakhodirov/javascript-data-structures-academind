959953375562a2e0d6ebf4221a3d85e98abddf15
Jakhongirkhoja-Bakhodirov <masternuxtjstest@gmail.com>
2022-04-17 23:21:55 +0500

    object:basic concepts

---
+++ objects/index.js
@@ -0,0 +1,12 @@
+const person = {
+    name:'John',
+    age:18,
+    gender:'male',
+    hobbies:['playing tennis' , 'walking' , 'riding']
+}
+
+console.log(person);
+
+delete person.age;
+
+console.log(person);
