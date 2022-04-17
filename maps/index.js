b7ec62070fd8ef17b7778a279a21c8e46496d00b
Jakhongirkhoja-Bakhodirov <masternuxtjstest@gmail.com>
2022-04-18 00:58:25 +0500

    maps:cloning and merging

--- maps/index.js
+++ maps/index.js
@@ -63,3 +63,50 @@ console.log([...myMapConvert]);
 //Or use the keys() or values() iterators,and convert them to an array
 console.log(Array.from(myMap.values()));
 console.log(Array.from(myMap.keys()));
+
+//Cloning and merging Maps
+const original = new Map([
+    [1, 'one']
+  ])
+
+const clone = new Map(original)
+
+console.log(clone.get(1))       // one
+console.log(original === clone) // false (useful for shallow comparison)
+
+const first = new Map([
+    [1, 'one'],
+    [2, 'two'],
+    [3, 'three'],
+  ])
+  
+  const second = new Map([
+    [1, 'uno'],
+    [2, 'dos']
+  ])
+  
+  // Merge two maps. The last repeated key wins.
+  // Spread operator essentially converts a Map to an Array
+  const merged = new Map([...first, ...second])
+  
+  console.log(merged.get(1)) // uno
+  console.log(merged.get(2)) // dos
+  console.log(merged.get(3)) // three
+
+  const firstMap = new Map([
+    [1, 'one'],
+    [2, 'two'],
+    [3, 'three'],
+  ])
+  
+  const secondMap = new Map([
+    [1, 'uno'],
+    [2, 'dos']
+  ])
+  
+  // Merge maps with an array. The last repeated key wins.
+  const mergedMap = new Map([...firstMap, ...secondMap, [1, 'eins']])
+  
+  console.log(mergedMap.get(1)) // eins
+  console.log(mergedMap.get(2)) // dos
+  console.log(mergedMap.get(3)) // three
