1503372906328f5a4efbafd0e8c7fbdb5e76fa6f
Jakhongirkhoja-Bakhodirov <masternuxtjstest@gmail.com>
2022-04-17 09:13:44 +0500

    arrays in js

---
+++ arrays/index.js
@@ -0,0 +1,16 @@
+const names = ['max' , 'manu' , 'julie' , 'Max'];
+const ages = [12,23,33,-34];
+
+// console.log(names);
+
+const fullData = names.concat(ages);
+
+// names.forEach(el => console.log(el));
+
+// for(const el of names) {
+//     console.log(el);
+// }
+
+console.log(ages.sort());
+
+console.log(Array.isArray(fullData));
